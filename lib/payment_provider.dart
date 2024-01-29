a926ef1c319580f84732ad08ae8cd04b9777af6c
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-01-29 11:19:30 +0100

    implement coin selector handling

--- lib/payment_provider.dart
+++ lib/payment_provider.dart
@@ -1,10 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter_gpiod/flutter_gpiod.dart';
+
 class PaymentProvider {
-  Stream<double> payment() {
-    return Stream.fromFutures([
-      Future.delayed(const Duration(seconds: 1), () => 0.2),
-      Future.delayed(const Duration(seconds: 3), () => 0.3),
-      Future.delayed(const Duration(seconds: 4), () => 0.4),
-      Future.delayed(const Duration(seconds: 6), () => 0.6)
-    ]);
+  Stream<double> payment(double price) {
+    late StreamController<double> controller;
+    late StreamSubscription eventListener;
+
+    final pulsePin = FlutterGpiod.instance.chips[0].lines[26];
+    double payed = 0;
+
+    void readCoinSelector() {
+      const List<double> coinMapping = [
+        0.0,
+        0.05,
+        0.10,
+        0.20,
+        0.50,
+        1.00,
+        2.00
+      ];
+
+      int impulses = 0;
+
+      eventListener = pulsePin.onEvent
+          .where((event) => event.edge == SignalEdge.rising)
+          .timeout(
+        const Duration(milliseconds: 136),
+        onTimeout: (sink) {
+          if (impulses > 0) {
+            final coinValue = coinMapping[(impulses / 2).floor()];
+            controller.add(payed += coinValue);
+          }
+
+          sink.close();
+          readCoinSelector();
+        },
+      ).listen((event) {
+        ++impulses;
+      });
+    }
+
+    void claimCoinSelector() {
+      pulsePin.requestInput(
+        consumer: "COIN",
+        triggers: {SignalEdge.falling},
+        bias: Bias.pullUp,
+      );
+
+      readCoinSelector();
+    }
+
+    void releaseCoinSelector() {
+      eventListener.cancel();
+      pulsePin.release();
+    }
+
+    controller = StreamController(
+      onCancel: releaseCoinSelector,
+      onPause: releaseCoinSelector,
+      onResume: claimCoinSelector,
+      onListen: claimCoinSelector,
+    );
+
+    return controller.stream;
   }
 }
