2ee9c0e4545e90eb51c61af7ddded69439e710a8
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-01-31 20:49:58 +0100

    format label design unit test prettier

--- test/model/label_design/toTextStyle.welltested_test.dart
+++ test/model/label_design/toTextStyle.welltested_test.dart
@@ -4,33 +4,37 @@ import 'package:vendo/model/label_design.dart';
 
 void main() {
   group('LabelDesign', () {
-    test('toTextStyle should return correct TextStyle for valid LabelDesign',
-        () {
-      const labelDesign = LabelDesign(
+    test(
+      'toTextStyle should return correct TextStyle for valid LabelDesign',
+      () {
+        const labelDesign = LabelDesign(
           fontFamily: 'Roboto',
           fontWeight: FontWeight.bold,
           fontSize: 16,
-          color: Colors.red);
+          color: Colors.red,
+        );
 
-      final textStyle = labelDesign.toTextStyle();
+        final textStyle = labelDesign.toTextStyle();
 
-      expect(textStyle.fontFamily, 'Roboto');
-      expect(textStyle.fontWeight, FontWeight.bold);
-      expect(textStyle.fontSize, 16.0);
-      expect(textStyle.color, Colors.red);
-    });
+        expect(textStyle.fontFamily, 'Roboto');
+        expect(textStyle.fontWeight, FontWeight.bold);
+        expect(textStyle.fontSize, 16.0);
+        expect(textStyle.color, Colors.red);
+      },
+    );
 
     test(
-        'toTextStyle should return correct TextStyle for null values in LabelDesign',
-        () {
-      const labelDesign = LabelDesign();
+      'toTextStyle should return correct TextStyle for null values in LabelDesign',
+      () {
+        const labelDesign = LabelDesign();
 
-      final textStyle = labelDesign.toTextStyle();
+        final textStyle = labelDesign.toTextStyle();
 
-      expect(textStyle.fontFamily, null);
-      expect(textStyle.fontWeight, null);
-      expect(textStyle.fontSize, null);
-      expect(textStyle.color, null);
-    });
+        expect(textStyle.fontFamily, null);
+        expect(textStyle.fontWeight, null);
+        expect(textStyle.fontSize, null);
+        expect(textStyle.color, null);
+      },
+    );
   });
 }
