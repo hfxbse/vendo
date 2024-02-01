f5180b9707894a2115095d36fdc7699fc3031b53
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-01 18:23:32 +0100

    generate payment provider unit tests

--- test/payment_provider_test.dart
+++ test/payment_provider_test.dart
@@ -10,30 +10,39 @@ import 'payment_provider_test.mocks.dart';
 
 @GenerateMocks([CoinSelector])
 void main() {
-  test('Payed amount should add up', () {
-    final coinSelector = MockCoinSelector();
+  // test payment provider
+  test('When paying with coins', () async {
+    final mockCoinSelector = MockCoinSelector();
+    final paymentProvider = PaymentProvider(
+      mockCoinSelector,
+    );
 
-    final coins = Stream.fromIterable([0.05, 0.10, 0.20, 0.50, 1.00, 2.00]);
-
-    when(coinSelector.coins).thenAnswer((_) => coins);
-
-    final stream = PaymentProvider(coinSelector)
-        .payment(42)
-        .timeout(const Duration(milliseconds: 100));
-
-    const epsilon = 0.001;
-    expect(
-      stream,
-      emitsInOrder(
-        [
-          closeTo(0.05, epsilon),
-          closeTo(0.15, epsilon),
-          closeTo(0.35, epsilon),
-          closeTo(0.85, epsilon),
-          closeTo(1.85, epsilon),
-          closeTo(3.85, epsilon),
+    when(mockCoinSelector.coins).thenAnswer(
+      (_) => Stream.fromIterable(
+        const [
+          0.05,
+          0.10,
+          0.20,
+          0.50,
+          1.00,
+          2.00,
         ],
       ),
     );
+
+    final payments = paymentProvider.payment(1.00);
+
+    // extract delta into a constant
+    const delta = 0.001;
+    expect(
+        payments,
+        emitsInOrder([
+          closeTo(0.05, delta),
+          closeTo(0.15, delta),
+          closeTo(0.35, delta),
+          closeTo(0.85, delta),
+          closeTo(1.85, delta),
+          closeTo(3.85, delta),
+        ]));
   });
 }
