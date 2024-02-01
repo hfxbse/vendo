db68fcc5170c09c8f41a320721236957e963964b
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-01 16:20:49 +0100

    fix label design unit tests

--- test/model/label_design_test.dart
+++ test/model/label_design_test.dart
@@ -9,12 +9,7 @@ void main() {
         fontFamily: "Roboto",
         fontSize: 12,
         fontWeight: FontWeight.w500,
-        fontStyle: FontStyle.italic,
-        letterSpacing: 1.5,
-        wordSpacing: 2.5,
-        height: 1.5,
         color: Colors.red,
-        backgroundColor: Colors.green,
       );
 
       final textStyle = labelDesign.toTextStyle();
@@ -22,12 +17,7 @@ void main() {
       expect(textStyle.fontFamily, labelDesign.fontFamily);
       expect(textStyle.fontSize, labelDesign.fontSize);
       expect(textStyle.fontWeight, labelDesign.fontWeight);
-      expect(textStyle.fontStyle, labelDesign.fontStyle);
-      expect(textStyle.letterSpacing, labelDesign.letterSpacing);
-      expect(textStyle.wordSpacing, labelDesign.wordSpacing);
-      expect(textStyle.height, labelDesign.height);
       expect(textStyle.color, labelDesign.color);
-      expect(textStyle.backgroundColor, labelDesign.backgroundColor);
     });
 
     test('When no values are provided', () {
@@ -41,7 +31,7 @@ void main() {
       expect(textStyle.fontStyle, null);
       expect(textStyle.letterSpacing, null);
       expect(textStyle.wordSpacing, null);
-      expect(textStyle.height, null);
+      expect(textStyle.height, 1);
       expect(textStyle.color, null);
       expect(textStyle.backgroundColor, null);
     });
