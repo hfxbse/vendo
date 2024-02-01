17687779ee387e7756a423a89933a80f74058c3f
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-01 16:19:59 +0100

    generate label design unit tests

--- test/model/label_design_test.dart
+++ test/model/label_design_test.dart
@@ -5,28 +5,45 @@ import 'package:vendo/model/label_design.dart';
 void main() {
   group('TextStyle should be created from LabelDesign', () {
     test('When values are provided', () {
-      const fontFamily = "Roboto";
-      const fontWeight = FontWeight.w100;
-      const double fontSize = 100;
-      const color = Colors.pink;
+      const labelDesign = LabelDesign(
+        fontFamily: "Roboto",
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        fontStyle: FontStyle.italic,
+        letterSpacing: 1.5,
+        wordSpacing: 2.5,
+        height: 1.5,
+        color: Colors.red,
+        backgroundColor: Colors.green,
+      );
 
-      final textStyle = const LabelDesign(
-        color: color,
-        fontWeight: fontWeight,
-        fontSize: fontSize,
-        fontFamily: fontFamily,
-      ).toTextStyle();
+      final textStyle = labelDesign.toTextStyle();
 
-      expect(textStyle.fontFamily, fontFamily);
-      expect(textStyle.fontWeight, fontWeight);
-      expect(textStyle.fontSize, fontSize);
-      expect(textStyle.color, color);
+      expect(textStyle.fontFamily, labelDesign.fontFamily);
+      expect(textStyle.fontSize, labelDesign.fontSize);
+      expect(textStyle.fontWeight, labelDesign.fontWeight);
+      expect(textStyle.fontStyle, labelDesign.fontStyle);
+      expect(textStyle.letterSpacing, labelDesign.letterSpacing);
+      expect(textStyle.wordSpacing, labelDesign.wordSpacing);
+      expect(textStyle.height, labelDesign.height);
+      expect(textStyle.color, labelDesign.color);
+      expect(textStyle.backgroundColor, labelDesign.backgroundColor);
     });
 
     test('When no values are provided', () {
-      final textStyle = const LabelDesign().toTextStyle();
+      const labelDesign = LabelDesign();
 
-      expect(textStyle, anything);
+      final textStyle = labelDesign.toTextStyle();
+
+      expect(textStyle.fontFamily, null);
+      expect(textStyle.fontSize, null);
+      expect(textStyle.fontWeight, null);
+      expect(textStyle.fontStyle, null);
+      expect(textStyle.letterSpacing, null);
+      expect(textStyle.wordSpacing, null);
+      expect(textStyle.height, null);
+      expect(textStyle.color, null);
+      expect(textStyle.backgroundColor, null);
     });
   });
 }
