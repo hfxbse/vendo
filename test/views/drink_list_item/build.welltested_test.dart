29e3a73dee554b35e3652deefe09ff29d438f49d
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-02-01 14:22:00 +0100

    fix generated drink list item test cases

--- test/views/drink_list_item/build.welltested_test.dart
+++ test/views/drink_list_item/build.welltested_test.dart
@@ -10,53 +10,89 @@ import 'package:vendo/views/drink_list_item.dart';
 
 @GenerateMocks([Drink, Brand, LabelDesign, BuildContext])
 void main() {
-  group('DrinkListItem', () {
-    testWidgets('should display drink brand name and drink name',
-        (WidgetTester tester) async {
-      final mockDrink = MockDrink();
-      final mockBrand = MockBrand();
-      final mockLabelDesign = MockLabelDesign();
+  late MemoryImage testImage;
 
-      when(mockDrink.brand).thenReturn(mockBrand);
-      when(mockBrand.name).thenReturn('Coca Cola');
-      when(mockDrink.name).thenReturn('Coke');
-      when(mockBrand.labelDesign).thenReturn(mockLabelDesign);
-      when(mockLabelDesign.toTextStyle()).thenReturn(TextStyle());
+  setUp(() async {
+    final image = await createTestImage(
+      width: 156,
+      height: 250,
+      cache: true,
+    );
 
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          body: DrinkListItem(drink: mockDrink),
-        ),
-      ));
-
-      expect(find.text('Coca Cola'), findsOneWidget);
-      expect(find.text('Coke'), findsOneWidget);
-    });
-
-    testWidgets('should handle long drink brand name and drink name',
-        (WidgetTester tester) async {
-      final mockDrink = MockDrink();
-      final mockBrand = MockBrand();
-      final mockLabelDesign = MockLabelDesign();
-
-      when(mockDrink.brand).thenReturn(mockBrand);
-      when(mockBrand.name)
-          .thenReturn('Coca Cola Coca Cola Coca Cola Coca Cola Coca Cola');
-      when(mockDrink.name)
-          .thenReturn('Coke Coke Coke Coke Coke Coke Coke Coke Coke Coke');
-      when(mockBrand.labelDesign).thenReturn(mockLabelDesign);
-      when(mockLabelDesign.toTextStyle()).thenReturn(TextStyle());
-
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          body: DrinkListItem(drink: mockDrink),
-        ),
-      ));
-
-      expect(find.text('Coca Cola Coca Cola Coca Cola Coca Cola Coca Cola'),
-          findsOneWidget);
-      expect(find.text('Coke Coke Coke Coke Coke Coke Coke Coke Coke Coke'),
-          findsOneWidget);
-    });
+    testImage = MemoryImage((await image.toByteData())!.buffer.asUint8List());
   });
+
+  group(
+    'DrinkListItem',
+    () {
+      testWidgets(
+        'should display drink brand name and drink name',
+        (WidgetTester tester) async {
+          final mockDrink = MockDrink();
+          final mockBrand = MockBrand();
+          final mockLabelDesign = MockLabelDesign();
+
+          when(mockDrink.brand).thenReturn(mockBrand);
+          when(mockBrand.name).thenReturn('Coca Cola');
+          when(mockDrink.name).thenReturn('Coke');
+          when(mockDrink.labelDesign).thenReturn(mockLabelDesign);
+          when(mockDrink.tileBackground).thenReturn(
+            const BoxDecoration(color: Colors.red),
+          );
+          when(mockDrink.bottleHead).thenReturn(testImage);
+
+          when(mockBrand.labelDesign).thenReturn(mockLabelDesign);
+          when(mockLabelDesign.toTextStyle()).thenReturn(const TextStyle());
+
+          await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+              body: DrinkListItem(drink: mockDrink),
+            ),
+          ));
+
+          expect(find.text('Coca Cola'), findsOneWidget);
+          expect(find.text('Coke'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should handle long drink brand name and drink name',
+        (WidgetTester tester) async {
+          final mockDrink = MockDrink();
+          final mockBrand = MockBrand();
+          final mockLabelDesign = MockLabelDesign();
+
+          when(mockDrink.brand).thenReturn(mockBrand);
+          when(mockBrand.name).thenReturn(
+            'Coca Cola Coca Cola Coca Cola Coca Cola Coca Cola',
+          );
+          when(mockDrink.name).thenReturn(
+            'Coke Coke Coke Coke Coke Coke Coke Coke Coke Coke',
+          );
+          when(mockDrink.tileBackground).thenReturn(
+            const BoxDecoration(color: Colors.red),
+          );
+          when(mockDrink.bottleHead).thenReturn(testImage);
+          when(mockDrink.labelDesign).thenReturn(mockLabelDesign);
+          when(mockBrand.labelDesign).thenReturn(mockLabelDesign);
+          when(mockLabelDesign.toTextStyle()).thenReturn(const TextStyle());
+
+          await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+              body: DrinkListItem(drink: mockDrink),
+            ),
+          ));
+
+          expect(
+            find.text('Coca Cola Coca Cola Coca Cola Coca Cola Coca Cola'),
+            findsOneWidget,
+          );
+          expect(
+            find.text('Coke Coke Coke Coke Coke Coke Coke Coke Coke Coke'),
+            findsOneWidget,
+          );
+        },
+      );
+    },
+  );
 }
